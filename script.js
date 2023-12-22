400ca804b0fdb4f2c1799819f287cde7db89f636
nilegamesclub <150048598+nilegamesclub@users.noreply.github.com>
2023-12-22 11:10:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,4 +22,25 @@ function nextSlide() {
 
 }
 
+
+
 setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
+
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
